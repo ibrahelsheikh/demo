5127ee496c1813109b119ba432f757f5ddb95326
Ibrahim Elsheikh <ibrahimelsheik2020@gmail.com>
2025-12-23 17:29:15 +0200

    init commit

--- src/main/java/com/example/demo/dto/request/CreateTimeTableRequest.java
+++ src/main/java/com/example/demo/dto/request/CreateTimeTableRequest.java
@@ -1,13 +1,17 @@
 package com.example.demo.dto.request;
 
 import com.example.demo.dto.general.DaySchedulerDto;
+import com.example.demo.emuns.Grade;
+import com.example.demo.emuns.Semester;
 
+import java.util.Date;
 import java.util.List;
 
 public record CreateTimeTableRequest (
 
-
-
+        Grade grade,
+        Semester semester,
+        Date date,
 
         List<DaySchedulerDto> DaysScheduler
 
