be1a71610b57da748780320f7cc4875690b86b80
Ibrahim Elsheikh <ibrahimelsheik2020@gmail.com>
2025-12-24 02:22:52 +0200

    init commit

--- src/main/java/com/example/demo/controller/home.java
+++ src/main/java/com/example/demo/controller/home.java
@@ -33,6 +33,28 @@ public class home {
     }
 
 
+    @GetMapping("/teachers")
+    public ResponseEntity<?> getAllTeachers() {
+        return new ResponseEntity<>(teacherService.getAllTeachers(), HttpStatus.OK);
+    }
+
+
+    @DeleteMapping("/teachers/{teacherId}")
+    public ResponseEntity<?> deleteTeacher(@PathVariable Long teacherId) {
+        teacherService.deleteTeacher(teacherId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+
+    @PutMapping("/teachers/{teacherId} ")
+    public ResponseEntity<?> updateTeacher(
+            @Valid @RequestBody UpdateTeacherRequest updateTeacherRequest
+    ) {
+        teacherService.updateTeacher(updateTeacherRequest);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+
     @PostMapping("/subjects")
     public ResponseEntity<URI> createSubject(
             @Valid @RequestBody CreateSubjectRequest createSubjectRequest
@@ -41,26 +63,6 @@ public class home {
         return ResponseEntity
                 .created(URI.create("/subjects/" + savedSubjectId))
                 .build();
-    }
-
-
-    @GetMapping("/teachers")
-    public ResponseEntity<?> getAllTeachers() {
-        return new ResponseEntity<>(teacherService.getAllTeachers(), HttpStatus.OK);
-    }
-
-    @DeleteMapping("/teachers/{teacherId}")
-    public ResponseEntity<?> deleteTeacher(@PathVariable Long teacherId) {
-        teacherService.deleteTeacher(teacherId);
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-    }
-
-    @PutMapping("/teachers/{teacherId} ")
-    public ResponseEntity<?> updateTeacher(
-            @Valid @RequestBody UpdateTeacherRequest updateTeacherRequest
-    ) {
-        teacherService.updateTeacher(updateTeacherRequest);
-        return new ResponseEntity<>(HttpStatus.OK);
     }
 
 
@@ -77,4 +79,85 @@ public class home {
     }
 
 
+//    @PutMapping("/subjects/{subjectId} ")
+//    public ResponseEntity<?> updateSubject(
+//            @Valid @RequestBody CreateSubjectRequest createSubjectRequest
+//    ) {
+//        subjectService.updateSubject(createSubjectRequest);
+//        return new ResponseEntity<>(HttpStatus.OK);
+//    }
+//
+//
+//    @PostMapping("/teachers/{teacherId}/subjects/{subjectId} ")
+//    public ResponseEntity<?> assignSubjectToTeacher(
+//            @PathVariable Long teacherId,
+//            @PathVariable Long subjectId
+//    ) {
+//        teacherService.assignSubjectToTeacher(teacherId, subjectId);
+//        return new ResponseEntity<>(HttpStatus.OK);
+//    }
+//
+//
+//    @PostMapping("/teachers/{teacherId}/remove-subject/{subjectId} ")
+//    public ResponseEntity<?> removeSubjectFromTeacher(
+//            @PathVariable Long teacherId,
+//            @PathVariable Long subjectId
+//    ) {
+//        teacherService.removeSubjectFromTeacher(teacherId, subjectId);
+//        return new ResponseEntity<>(HttpStatus.OK);
+//    }
+//
+//
+//    @PostMapping("/teachers/{teacherId}/subjects")
+//    public ResponseEntity<?> assignMultipleSubjectsToTeacher(
+//            @PathVariable Long teacherId,
+//            @RequestBody Long[] subjectIds
+//    ) {
+//        teacherService.assignMultipleSubjectsToTeacher(teacherId, subjectIds);
+//        return new ResponseEntity<>(HttpStatus.OK);
+//    }
+//
+//
+//    // create endpoint to unassign multiple subjects from a teacher
+//    @PostMapping("/teachers/{teacherId}/remove-subjects")
+//    public ResponseEntity<?> removeMultipleSubjectsFromTeacher(
+//            @PathVariable Long teacherId,
+//            @RequestBody Long[] subjectIds
+//    ) {
+//        teacherService.removeMultipleSubjectsFromTeacher(teacherId, subjectIds);
+//        return new ResponseEntity<>(HttpStatus.OK);
+//    }
+
+
+    @PostMapping("/weekly-schedules")
+    public ResponseEntity<Void> createWeeklySchedule(
+            @RequestBody CreateWeeklyScheduleRequest request
+    ) {
+        Long id = weekSchedulerService.create(request);
+
+        return ResponseEntity
+                .created(URI.create("/weekly-schedules/" + id))
+                .build();
+    }
+
+
+
+
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
