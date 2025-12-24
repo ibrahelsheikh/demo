e62049a28d39cd26ce35e102bd75fe3dab64cc57
Ibrahim Elsheikh <ibrahimelsheik2020@gmail.com>
2025-12-24 21:43:38 +0200

    init commit

--- src/main/java/com/mega/dev/SchoolScheduleApplication.java
+++ src/main/java/com/mega/dev/SchoolScheduleApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class SchoolScheduleApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(SchoolScheduleApplication.class, args);
     }
 
 }
